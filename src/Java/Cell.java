b3537c6f2420e884e5592157338149b9533146bf
Maksim Dudnik <dudnikm1015@gmail.com>
2021-06-11 00:42:01 -0700

    Added functionality
    Program checks cells' neighbours and creates next generations

--- src/Java/Cell.java
+++ src/Java/Cell.java
@@ -3,41 +3,32 @@ package Java;
 import java.awt.*;
 
 public class Cell {
-    private boolean alive;
     private Color color;
-    private int numberOfAliveNeighbours;
 
     public Cell(boolean initialState){
         if(initialState) {
-            this.alive = true;
             this.color = Color.white;
         } else {
-            this.alive = false;
             this.color = Color.black;
         }
     }
 
-    public boolean isAlive() {
-        return alive;
-    }
-
-    public void setAlive(boolean alive) {
-        this.alive = alive;
-    }
 
     public Color getColor() {
         return color;
     }
 
-    public void setColor(Color color) {
-        this.color = color;
+    public void getNextState(int aliveNeighbours){
+        if(color == Color.black) {
+            if (aliveNeighbours == 3)
+                changeState();
+        } else {
+            if(aliveNeighbours < 2 || aliveNeighbours > 3)
+                changeState();
+        }
     }
 
-    public int getNumberOfAliveNeighbours() {
-        return numberOfAliveNeighbours;
-    }
-
-    public void setNumberOfAliveNeighbours(int numberOfAliveNeighbours) {
-        this.numberOfAliveNeighbours = numberOfAliveNeighbours;
+    public void changeState(){
+        color = (color == Color.black ? Color.white : Color.black);
     }
 }
--- src/Java/MainPanel.java
+++ src/Java/MainPanel.java
@@ -4,6 +4,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.util.Random;
 
 public class MainPanel extends JPanel implements ActionListener {
@@ -11,7 +13,7 @@ public class MainPanel extends JPanel implements ActionListener {
     public static int SCREEN_WIDTH = 500;
     public static int SCREEN_HEIGHT = 500;
     public static int DELAY = 150;
-    public static int SCREEN_UNIT = 20;
+    public static int SCREEN_UNIT = 25;
 
     private Timer timer;
     private boolean running;
@@ -23,6 +25,8 @@ public class MainPanel extends JPanel implements ActionListener {
         this.setBackground(Color.BLACK);
         this.setFocusable(true);
 
+        this.addKeyListener(new MyKeyAdapter());
+
         startGame();
     }
 
@@ -32,7 +36,7 @@ public class MainPanel extends JPanel implements ActionListener {
         r = new Random();
         for(int i =0; i<cells.length;i++){
             for(int j = 0; j < cells[0].length;j++){
-                if(r.nextInt(100) < 20) {
+                if(r.nextInt(100) < 10) {
                     cells[i][j] = new Cell(true);
                 } else {
                     cells[i][j] = new Cell(false);
@@ -50,19 +54,70 @@ public class MainPanel extends JPanel implements ActionListener {
     }
 
     public void draw(Graphics g){
+
+        //Draw grid
+
         for(int i =0; i<cells.length;i++){
             for(int j = 0; j < cells[0].length;j++){
                 g.setColor(cells[i][j].getColor());
-                g.fillRect(j*SCREEN_UNIT,i*SCREEN_UNIT,SCREEN_UNIT,SCREEN_UNIT);
+                g.fillRect(j*SCREEN_UNIT,i*SCREEN_UNIT,SCREEN_UNIT-1,SCREEN_UNIT-1);
             }
         }
+    }
+
+    public void restart(){
+        for(int i =0; i<cells.length;i++){
+            for(int j = 0; j < cells[0].length;j++){
+                if(r.nextInt(100) < 10) {
+                    cells[i][j] = new Cell(true);
+                } else {
+                    cells[i][j] = new Cell(false);
+                }
+            }
+        }
+    }
+
+    public void nextGeneration(){
+        int aliveNeighbours;
+        for(int i =0; i<cells.length;i++){
+            for(int j = 0; j < cells[0].length;j++){
+                aliveNeighbours = 0;
+                for(int k = -1; k<2;k++){
+                    if(i-1 >= 0 && j+k >= 0 && j+k < SCREEN_WIDTH/SCREEN_UNIT)
+                        if(cells[i-1][j+k].getColor() == Color.white)
+                            aliveNeighbours++;
+                    if(i+1 < SCREEN_HEIGHT/SCREEN_UNIT && j+k >= 0 && j+k < SCREEN_WIDTH/SCREEN_UNIT)
+                        if(cells[i+1][j+k].getColor() == Color.white)
+                            aliveNeighbours++;
+                }
+                if(j-1 >= 0 && j+1 < SCREEN_WIDTH/SCREEN_UNIT) {
+                    if (cells[i][j - 1].getColor() == Color.white)
+                        aliveNeighbours++;
+                    if(cells[i][j+1].getColor() == Color.white)
+                        aliveNeighbours++;
+                }
+                cells[i][j].getNextState(aliveNeighbours);
+                System.out.printf("i = %d , j = %d, # = %d\n",i,j,aliveNeighbours);
+            }
+        }
+
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         if(running){
-
+            nextGeneration();
         }
         repaint();
+    }
+
+    class MyKeyAdapter extends KeyAdapter{
+        @Override
+        public void keyPressed(KeyEvent e) {
+            if(e.getKeyCode() == KeyEvent.VK_SPACE)
+                running = !running;
+            if(e.getKeyCode() == KeyEvent.VK_W)
+                restart();
+        }
     }
 }
